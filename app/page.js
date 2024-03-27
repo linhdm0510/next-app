18b4c13d593f2f5143e1224995025ba8b56cc729
linh do <linhlinhdo0406@gmail.com>
2024-03-27 23:44:34 +0700

    fix(frontend): update validation commit message

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
+  const user = {name: "linh", age: 20}
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
